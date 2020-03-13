40c07db998c414115534b8826519b0343aa9350f
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2020-03-13 14:37:11 +0000

    Updated Number ranges for BISS (#46)

--- test/v1/models/response/common/TotalSpec.scala
+++ test/v1/models/response/common/TotalSpec.scala
@@ -49,13 +49,48 @@ class TotalSpec extends UnitSpec {
       expenses = Some(100.25)
     )
 
+  val mtdJsonWithNegativeValues: JsValue = Json.parse(
+    """
+      |{
+      | "income": 100.00,
+      | "additions" : -100.00,
+      | "deductions" : 100.25,
+      | "expenses" : -100.25
+      |}
+    """.stripMargin)
+
+  val desJsonWithNegativeValues: JsValue = Json.parse(
+    """
+      |{
+      | "totalIncome": 100.00,
+      | "totalAdditions" : -100.00,
+      | "totalDeductions" : 100.25,
+      | "totalExpenses" : -100.25
+      |}
+    """.stripMargin)
+
+  val modelWithNegativeValues =
+    Total(
+      income = 100.00,
+      additions = Some(-100.00),
+      deductions = Some(100.25),
+      expenses = Some(-100.25)
+    )
+
   "TaxBand" should {
 
     "write correctly to json" in {
       Json.toJson(model) shouldBe json
     }
+    "write correctly to json with negative values" in {
+      Json.toJson(modelWithNegativeValues) shouldBe mtdJsonWithNegativeValues
+    }
 
     "read correctly from json" in {
+      desJson.as[Total] shouldBe model
+    }
+
+    "read correctly from json having negative values" in {
       desJson.as[Total] shouldBe model
     }
   }
