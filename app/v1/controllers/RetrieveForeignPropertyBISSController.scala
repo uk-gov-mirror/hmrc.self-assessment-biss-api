f6b25820d6aa7cec6de8dcc4687f85564dc6f62d
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2021-01-29 11:20:59 +0000

    Updated logging levels settings in the code and conf files (#64)

--- app/v1/controllers/RetrieveForeignPropertyBISSController.scala
+++ app/v1/controllers/RetrieveForeignPropertyBISSController.scala
@@ -70,7 +70,7 @@ class RetrieveForeignPropertyBISSController @Inject()(val authService: Enrolment
       result.leftMap { errorWrapper =>
         val resCorrelationId = errorWrapper.correlationId
         val result = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
-        logger.info(
+        logger.warn(
           s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
             s"Error response received with CorrelationId: $resCorrelationId")
 
