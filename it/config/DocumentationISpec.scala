519d49ca27c292669d862fda9dd531dba461b35b
HarryBaxter <52996647+HarryBaxter@users.noreply.github.com>
2022-04-25 13:45:33 +0100

    MTDSA-11693 Retirement of Biss V1 (#93)

--- it/config/DocumentationISpec.scala
+++ it/config/DocumentationISpec.scala
@@ -47,7 +47,7 @@ class DocumentationISpec extends IntegrationBaseSpec {
       |    "versions":[
       |      {
       |        "version":"1.0",
-      |        "status":"DEPRECATED",
+      |        "status":"RETIRED",
       |        "endpointsEnabled":true
       |      },
       |      {
