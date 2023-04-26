73e7fefa82fb0e17639f7ac1c16eebb784394aae
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-04-26 16:04:03 +0100

    MTDSA-16247: Moved RequestParserSpec

--- test/api/controllers/requestParsers/RequestParserSpec.scala
+++ test/api/controllers/requestParsers/RequestParserSpec.scala
@@ -14,9 +14,8 @@
  * limitations under the License.
  */
 
-package v2.controllers.requestParsers
+package api.controllers.requestParsers
 
-import api.controllers.requestParsers.RequestParser
 import api.controllers.requestParsers.validators.Validator
 import api.models.domain.Nino
 import api.models.errors.{BadRequestError, ErrorWrapper, NinoFormatError, RuleIncorrectOrEmptyBodyError}
